f72a5ec820d45ebbb39ad4f388e6548091b83612
Tarun Naidu G <welcometokannada2017@gmail.com>
2024-01-11 10:57:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(__dirname +'/public/'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
